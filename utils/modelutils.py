178c4ee1f44c60eaff07514e8a7990a1cacaf82a
Tsai-chia-hsiang <u08410098@ccu.edu.tw>
2023-07-28 22:11:51 +0800

    n

--- utils/modelutils.py
+++ utils/modelutils.py
@@ -1,7 +1,6 @@
 import os
 import torch
 import torch.nn as nn
-import torchvision
 from torchvision.models import resnet50, ResNet50_Weights
 
 class TransferResNet50(nn.Module):
@@ -26,12 +25,12 @@ class TransferResNet50(nn.Module):
         self.rn50.fc = nn.Sequential(*fclayers)
         
     def forward(self, x:torch.Tensor):
-        return self.rn50.forward(x)
+        return self.rn50(x)
 
 
 def dowload_pretrainrn50(savepath):
 
-    rn50 = resnet50(weights=torchvision.models.ResNet50_Weights.IMAGENET1K_V2)
+    rn50 = resnet50(weights=ResNet50_Weights.IMAGENET1K_V2)
     torch.save(rn50.state_dict(),f=savepath)
 
 def transfer_rn50_model(pretrained_path:os.PathLike, FC:list)->TransferResNet50:
