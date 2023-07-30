e6649c0b3ddc1b49441b2d8fbb304a79df56e35d
Tsai-chai-hsiang <u08410098@ccu.edu.tw>
2023-07-31 01:16:48 +0800

    c

--- utils/pathutils.py
+++ utils/pathutils.py
@@ -1,9 +1,10 @@
 import os
+import os.path as osp
 import shutil
 
 def makedir(d:os.PathLike, rmold=False):
     
-    if os.path.exists(d):
+    if osp.exists(d):
         if rmold :
             shutil.rmtree(d)
         else:
@@ -15,13 +16,13 @@ def makedir(d:os.PathLike, rmold=False):
 def check_dir(root:os.PathLike, necessary_subfolers_files:list = None)->os.PathLike:
     nsf = [] if necessary_subfolers_files is None else necessary_subfolers_files
     for i in nsf:
-        assert os.path.exists(os.path.join(root, i))
+        assert osp.exists(osp.join(root, i))
         
     return root
 
 
 def get_newest_index_subdir(root:os.PathLike,based_idx:int, prefix="")->int:
     bidx = based_idx
-    while(os.path.exists(os.path.join(root, f"{prefix}{bidx}"))):
+    while(osp.exists(osp.join(root, f"{prefix}{bidx}"))):
         bidx += 1
     return bidx
